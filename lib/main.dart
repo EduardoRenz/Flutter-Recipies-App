70ebe49b57f8e1e0e25c63649399dcd7ec51fe43
Eduardo <dudarenz@gmail.com>
2022-01-25 04:03:12 -0300

    Added filtering meals

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:recipies/models/meal.dart';
+import 'package:recipies/models/settings.dart';
 import 'package:recipies/screens/meal_detail_screen.dart';
 import 'package:recipies/screens/settings_screen.dart';
 import 'package:recipies/screens/tabs_screen.dart';
 import 'package:recipies/utils/app_routes.dart';
+import 'data/dummy_data.dart';
 import 'screens/categories_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Meal> _avaliableMeals = DUMMY_MEALS;
+
+  void _filterMeals(Settings settings) {
+    setState(() {
+      _avaliableMeals = DUMMY_MEALS.where((meal) {
+        final filterGlutten = settings.isGlutenFree && !meal.isGlutenFree;
+        final filterLactose = settings.isLactoseFree && !meal.isLactoseFree;
+        final filterVegan = settings.isVegan && !meal.isVegan;
+        final filterVegetarian = settings.isVegetarian && !meal.isVegetarian;
+        return !filterGlutten &&
+            !filterLactose &&
+            !filterVegan &&
+            !filterVegetarian;
+      }).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,9 +50,10 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         AppRoutes.HOME: (ctx) => const TabsScreen(),
-        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) =>
+            CategoriesMealsScreen(_avaliableMeals),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
-        AppRoutes.SETTINGS: (ctx) => SettingsScreen(),
+        AppRoutes.SETTINGS: (ctx) => SettingsScreen(_filterMeals),
       },
     );
   }
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,17 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:recipies/components/main_drawer.dart';
+import 'package:recipies/models/settings.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
+  final Function(Settings settings) onSettingsChanged;
+
+  const SettingsScreen(this.onSettingsChanged);
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  Settings settings = Settings();
+
+  Widget _createSwitch(
+      String title, String subtitle, bool value, Function(bool) onChanged) {
+    return SwitchListTile.adaptive(
+        title: Text(title),
+        subtitle: Text(subtitle),
+        value: value,
+        onChanged: (value) {
+          onChanged(value);
+          widget.onSettingsChanged(settings);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      drawer: MainDrawer(),
-      body: const Center(
-        child: Text('Settings'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Settings'),
+        ),
+        drawer: MainDrawer(),
+        body: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                'Settings',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ),
+            Expanded(
+                child: ListView(
+              children: [
+                _createSwitch(
+                  'Gluten-free',
+                  'Only include gluten-free recipes',
+                  settings.isGlutenFree,
+                  (newValue) {
+                    setState(() {
+                      settings.isGlutenFree = newValue;
+                    });
+                  },
+                ),
+                _createSwitch(
+                  'Lactose-free',
+                  'Only include lactose-free recipes',
+                  settings.isLactoseFree,
+                  (newValue) {
+                    setState(() {
+                      settings.isLactoseFree = newValue;
+                    });
+                  },
+                ),
+                _createSwitch(
+                  'Vegetarian',
+                  'Only include vegetarian recipes',
+                  settings.isVegetarian,
+                  (newValue) {
+                    setState(() {
+                      settings.isVegetarian = newValue;
+                    });
+                  },
+                ),
+                _createSwitch(
+                  'Vegan',
+                  'Only include vegan recipes',
+                  settings.isVegan,
+                  (newValue) {
+                    setState(() {
+                      settings.isVegan = newValue;
+                    });
+                  },
+                ),
+              ],
+            ))
+          ],
+        ));
   }
 }
